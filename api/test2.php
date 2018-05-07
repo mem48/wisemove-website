a26b3b072e8197f68b7005e177436f53d11bbf64
Malcolm Morgan <mem48@users.noreply.github.com>
2018-05-07 12:29:40 +0100

    next test

--- api/test2.php
+++ api/test2.php
@@ -4,13 +4,28 @@
  </head>
  <body>
  <?php
-	echo "Running Tests v2";
-	include_once ('./defaults.php');
-	$defaults = defaults ();
-	echo $defaults;
-	echo "Getting Settings";
-	$this->settings = $this->defaults ();
-	echo $this;
+	echo "Running Tests v3";
+	include_once ('./connect.php');
+	// Performing SQL query
+	$query = 'SELECT * FROM zones LIMIT 5';
+	$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+	// Printing results in HTML
+	echo "<table>\n";
+	while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+		echo "\t<tr>\n";
+		foreach ($line as $col_value) {
+			echo "\t\t<td>$col_value</td>\n";
+		}
+		echo "\t</tr>\n";
+	}
+	echo "</table>\n";
+
+	// Free resultset
+	pg_free_result($result);
+
+	// Closing connection
+	pg_close($dbconn);
  ?>
  </body>
 </html>
