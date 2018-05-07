a33967d7848624ea0bdc262c92f3c6d3737c2c4a
Malcolm Morgan <mem48@users.noreply.github.com>
2018-05-07 10:32:06 +0100

    testing api

---
+++ api/test.php
@@ -0,0 +1,58 @@
+<?php
+# Class properties
+private $databaseConnection;
+
+# Supported formats
+private $formats = array ('json', 'geojson', 'csv');
+
+
+# Defaults
+private function defaults ()
+{
+	return array (
+	'hostname' => 'localhost',
+		'username' => 'wisemove',
+		'password' => NULL,		// Postgres peer connection type is being used
+		'database' => 'wisemove',
+	);
+}
+	
+	
+# Constructor
+public function __construct ()
+{
+	# Load settings
+	$this->settings = $this->defaults ();
+	
+	# Load subclass
+	require_once ('./wisemoveModel.php');
+	
+	echo $this
+		
+	# Connect to the database, providing a DSN connection string in this format: 'pgsql:host=localhost;dbname=example'
+	try {
+		$this->databaseConnection = new PDO ("pgsql:host={$this->settings['hostname']};dbname={$this->settings['database']}", $this->settings['username'], $this->settings['password']);
+		$this->databaseConnection->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$this->databaseConnection->setAttribute (PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	} catch (PDOException $e) {
+		// var_dump ($e->getMessage (), $query);
+		return $this->error ('Unable to connect to the database.');
+	}
+	
+	# Ensure a valid format has been supplied
+	$format = $this->getFormat ($error);
+	if ($error) {
+		return $this->error ($error);
+	}
+	
+		
+	# Load the model, passing in API parameters
+	$this->wisemoveModel = new wisemoveModel ($bbox, $zoom, $_GET);
+	
+	# Ensure a valid action has been supplied
+	$method = $this->getMethod ($error);
+	if ($error) {
+		return $this->error ($error);
+	}
+}
+?>
