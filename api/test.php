e681c340aca0863f8580934888977c06cee923a8
Malcolm Morgan <mem48@users.noreply.github.com>
2018-05-07 11:23:16 +0100

    take out function

--- api/test.php
+++ api/test.php
@@ -2,6 +2,7 @@
 # Load and run the API class
 require_once ('./defaults.php');
 
+echo "Running Tests"
 
 # Class properties
 private $databaseConnection;
@@ -10,41 +11,38 @@ private $databaseConnection;
 private $formats = array ('json', 'geojson', 'csv');
 
 	
-# Constructor
-public function __construct ()
-{
-	# Load settings
-	$this->settings = $this->defaults ();
+
+# Load settings
+$this->settings = $this->defaults ();
+
+# Load subclass
+require_once ('./wisemoveModel.php');
+
+echo $this
 	
-	# Load subclass
-	require_once ('./wisemoveModel.php');
+# Connect to the database, providing a DSN connection string in this format: 'pgsql:host=localhost;dbname=example'
+try {
+	$this->databaseConnection = new PDO ("pgsql:host={$this->settings['hostname']};dbname={$this->settings['database']}", $this->settings['username'], $this->settings['password']);
+	$this->databaseConnection->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$this->databaseConnection->setAttribute (PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+	// var_dump ($e->getMessage (), $query);
+	return $this->error ('Unable to connect to the database.');
+}
+
+# Ensure a valid format has been supplied
+$format = $this->getFormat ($error);
+if ($error) {
+	return $this->error ($error);
+}
+
 	
-	echo $this
-		
-	# Connect to the database, providing a DSN connection string in this format: 'pgsql:host=localhost;dbname=example'
-	try {
-		$this->databaseConnection = new PDO ("pgsql:host={$this->settings['hostname']};dbname={$this->settings['database']}", $this->settings['username'], $this->settings['password']);
-		$this->databaseConnection->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$this->databaseConnection->setAttribute (PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-	} catch (PDOException $e) {
-		// var_dump ($e->getMessage (), $query);
-		return $this->error ('Unable to connect to the database.');
-	}
-	
-	# Ensure a valid format has been supplied
-	$format = $this->getFormat ($error);
-	if ($error) {
-		return $this->error ($error);
-	}
-	
-		
-	# Load the model, passing in API parameters
-	$this->wisemoveModel = new wisemoveModel ($bbox, $zoom, $_GET);
-	
-	# Ensure a valid action has been supplied
-	$method = $this->getMethod ($error);
-	if ($error) {
-		return $this->error ($error);
-	}
+# Load the model, passing in API parameters
+$this->wisemoveModel = new wisemoveModel ($bbox, $zoom, $_GET);
+
+# Ensure a valid action has been supplied
+$method = $this->getMethod ($error);
+if ($error) {
+	return $this->error ($error);
 }
 ?>
